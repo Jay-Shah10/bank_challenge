2bf2da0db26b04bbd58b3ef76dcfd5aa6a0fb913
Jay Shah <j24shah@gmail.com>
2019-10-06 00:22:57 -0400

    added in post and patch.

--- app.py
+++ app.py
@@ -17,12 +17,35 @@ class User(Resource):
         user = synapse.get_user(user_id=user_id)
         return user
     
-    def post(self, user_id=None):
-        user = synapse.create_user(payload=request.get_json())
-        # update mongodb
+    def patch(self, user_id):
+        body = request.json
+        user = synapse.update_user_status(user_id=user_id, body=body)
         return user
 
+    @app.route('/bank/api/dev/users', methods=['POST'])
+    def post(self):
+        payload = request.json
+        user = synapse.create_user(payload=payload)
+        mongodb_client = MongoClient('localhost', 27107)
+        db = mongodb_client['bank']
+
+        entry = {
+            'user_id': payload['_id'], 
+            'document_id': payload['documents'][0]['id'],
+            'legal_name' : payload['legal_names'][0],
+            'refresh_token': payload['refresh_token']
+        }
+        # insert the entry into the database.
+        post = db.posts
+        post.insert_one(entry)
+        return user
+
+class Account(Resource):
+    def get(self)
+
+
 api.add_resource(User,'/bank/api/dev/users/<string:user_id>')
+
     
 
 if __name__ == "__main__":
--- client/synapse.py
+++ client/synapse.py
@@ -7,6 +7,7 @@ client_headers = {
     'X-SP-USER': f'|{configs.FINGERPRINT}',
     'X-SP-GATEWAY': f'{configs.CLIENT_ID}|{configs.CLIENT_SECRET}'
 }
+
 dev_url = 'https://uat-api.synapsefi.com/v3.1/'
 
 def get_users():
@@ -33,5 +34,31 @@ def create_user(payload):
             return response.json()
         else:
             return response.json()
+    except Exception as exc:
+        return exc
+
+def update_user_status(user_id, body):
+    user_url = f"{dev_url}/users/{user_id}"
+    try:
+        response = requests.patch(url=user_url,
+                                 headers=client_headers, 
+                                 data = body,
+                                 verify=False)
+        return response.json()
+    except Exception as exc:
+        return exc
+
+
+def get_oauth(user_id, payload):
+    res = get_user(user_id=id)
+    res.json()['refresh_token']
+
+    oauth = f"{dev_url}/oauth/{user_id}"
+    try:
+        response = requests.post(url=oauth, 
+                                headers=client_headers,
+                                data=payload, 
+                                verify=False)
+        return response.json()['oauth_key']
     except Exception as exc:
         return exc
